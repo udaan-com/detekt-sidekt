d15f1ee6af0901361ba40bfd2ddfdd77f5faabff
chandan-satapathy <chandan.satapathy@udaan.com>
2024-09-18 04:01:21 +0530

    refactor

--- src/main/kotlin/io/github/thewisenerd/linters/sidekt/rules/JsonIgnorePropertiesOnDataClass.kt
+++ src/main/kotlin/io/github/thewisenerd/linters/sidekt/rules/JsonIgnorePropertiesOnDataClass.kt
@@ -1,13 +1,7 @@
 package io.github.thewisenerd.linters.sidekt.rules
 
 import io.github.thewisenerd.linters.sidekt.helpers.Debugger
-import io.gitlab.arturbosch.detekt.api.CodeSmell
-import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Debt
-import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.Rule
-import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.*
 import org.jetbrains.kotlin.psi.KtClass
 
 /**
